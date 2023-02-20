e1bf6441d9b7eda1558751825c54543dbab6687a
moatz.awad <moataz.3waad@gmail.com>
2023-02-20 14:44:09 +0200

    second commit

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import desktop_window
-import shared_preferences_macos
+import shared_preferences_foundation
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
